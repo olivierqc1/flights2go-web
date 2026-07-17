f5fff794741c690043751775b421b4a3f78f3c87
olivierqc1 <olivierturgeon@hotmail.fr>
2026-07-17 22:07:50 +0200

    Actualizar next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/flights2go-web',
-  assetPrefix: '/flights2go-web/',
   images: { unoptimized: true },
   trailingSlash: true,
 };
